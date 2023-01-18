70c7925c33431342c92fc56aaff581ee9be5e94d
majda khalifa <khalifamajda12@gmail.com>
2023-01-18 11:33:04 +0100

    commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO address (id, creation, content) VALUES (1, CURRENT_TIMESTAMP(), '57 boulevard demorieux');
+INSERT INTO address (id, creation, content) VALUES (2, CURRENT_TIMESTAMP(), '51 allee du gamay, 34080
+montpellier');
