5b31cafc7411d7ec130de304e76f89a38f46dda3
majda khalifa <khalifamajda12@gmail.com>
2023-01-18 11:48:18 +0100

    commit

---
+++ src/main/java/com/ensim/TP3/controller/AddressController.java
@@ -0,0 +1,19 @@
+package com.ensim.TP3.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.ensim.TP3.model.AddressRepository;
+
+@Controller
+public class AddressController {
+	@Autowired
+	AddressRepository addressRepository;
+	@GetMapping("/adresses")
+	public String showAddresses(Model model) {
+		model.addAttribute("allAddresses", addressRepository.findAll());
+		return "addresses";
+	}
+}
