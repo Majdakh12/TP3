4e67803905056079180a9c6523e6c29507254cfa
majda khalifa <khalifamajda12@gmail.com>
2023-01-18 12:06:07 +0100

    commit

--- src/main/java/com/ensim/TP3/controller/AddressController.java
+++ src/main/java/com/ensim/TP3/controller/AddressController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.ensim.TP3.model.AddressRepository;
 
@@ -12,8 +13,9 @@ public class AddressController {
 	@Autowired
 	AddressRepository addressRepository;
 	@GetMapping("/adresses")
-	public String showAddresses(Model model) {
+	public String showAddresses(@RequestParam("searcherName") String searcherName, Model model) {
 		model.addAttribute("allAddresses", addressRepository.findAll());
+		model.addAttribute("searcherName", searcherName);
 		return "addresses";
 	}
 }
