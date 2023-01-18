dd26956e38ea94df68cc01d941bcc6d7a5630de6
majda khalifa <khalifamajda12@gmail.com>
2023-01-18 11:30:05 +0100

    commit

--- src/main/java/com/ensim/TP3/model/Address.java
+++ src/main/java/com/ensim/TP3/model/Address.java
@@ -13,4 +13,24 @@ public class Address {
 	private Long id;
 	private Date creation;
 	private String content;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Date getCreation() {
+		return creation;
+	}
+	public void setCreation(Date creation) {
+		this.creation = creation;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+	
 }
