ab1c7d20b9095894be808ab1e759291731446061
majda khalifa <khalifamajda12@gmail.com>
2023-01-18 09:52:40 +0100

    first commit

---
+++ src/main/java/com/ensim/TP3/Tp3Application.java
@@ -0,0 +1,13 @@
+package com.ensim.TP3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ensim/TP3/Tp3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ensim.TP3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
