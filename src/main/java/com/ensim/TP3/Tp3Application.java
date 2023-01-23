4a01a0e3579048c5d0a3adc4945a5fc9afd13076
khalifamajda12@gmail.com <khalifamajda12@gmail.com>
2023-01-23 16:38:39 +0100

    commit

--- src/main/java/com/ensim/TP3/Tp3Application.java
+++ src/main/java/com/ensim/TP3/Tp3Application.java
@@ -2,12 +2,20 @@ package com.ensim.TP3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class Tp3Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Tp3Application.class, args);
+	}
+	
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
 	}
 
 }
