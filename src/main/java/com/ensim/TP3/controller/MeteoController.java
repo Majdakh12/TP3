050dfa295bdacd40e9bdf82d195eb06b8cfd9d9b
khalifamajda12@gmail.com <khalifamajda12@gmail.com>
2023-01-23 15:05:20 +0100

    commit

---
+++ src/main/java/com/ensim/TP3/controller/MeteoController.java
@@ -0,0 +1,29 @@
+package com.ensim.TP3.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.ensim.TP3.model.Address;
+import com.ensim.TP3.model.AddressRepository;
+
+@Controller
+public class MeteoController {
+	@Autowired
+    AddressRepository addressRepository;
+    
+    @GetMapping("/meteo")
+    public String weatherForm(Model model) {
+        model.addAttribute("address", new Address());
+        return "weatherForm";
+    }
+    
+    @PostMapping("/meteo")
+    public String weatherSubmit(@ModelAttribute Address address) {
+        addressRepository.save(address);
+        return "weatherResult";
+    }
+}
